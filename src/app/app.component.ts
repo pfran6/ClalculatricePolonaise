490d6eeb812c6ebbed6a8337c269d708a3ae570b
Francois <fr.passiller@laposte.net>
2019-04-17 11:02:31 +0200

    Commit 11h

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ClalculatricePolonaise';
+
+  operation: string[] = ['0', '0'];
+  number = '';
+  resultat: number;
+
+  clickCalculatrice() {
+    if (this.operation[0] === '0') {
+      this.operation[0] = this.number;
+    } else {
+      this.operation[1] = this.number;
+    }
+    this.number = '';
+  }
+
+  clickDel() {
+    this.operation = ['0', '0'];
+    this.number = '';
+  }
+
+  clickNumber(nb: string) {
+    this.number += nb;
+  }
+
+  additionner() {
+    this.resultat = +this.operation[0] + +this.operation[1];
+  }
 }
